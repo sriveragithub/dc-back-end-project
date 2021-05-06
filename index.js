206f3f43c07cc0fca3c0fab1f1025f32e2f0c2f1
Greenhouse89 <greenbrandon07@yahoo.com>
2021-05-06 19:02:51 -0400

    ES6 added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const es6Renderer = require('express-es6-template-engine');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
+  .engine('html', es6Renderer)
+  .set('views', 'views')
   .set('view engine', 'html')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
