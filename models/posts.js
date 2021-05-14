275ca43f4af8155a3fef342b0a6a26ef22136f8b
Eric Treadway <eric.s.treadway@gmail.com>
2021-05-14 09:04:11 -0500

    moving db to elephantSQL

---
+++ models/posts.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Posts.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    bio: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Posts',
+  });
+  return Posts;
+};
