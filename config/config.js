a245d0d0cf50f930e3962a2973e83011b8f49d9e
Skyler Rivera <skylerrivera17@yahoo.com>
2021-05-15 11:05:22 -0400

    adding config files and sequelizerc to look at env variables

---
+++ config/config.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+module.exports = 
+{
+  "development": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "hire_me",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
+  
